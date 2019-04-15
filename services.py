4c68e842f46069147f0d6ac58f449e48967c6585
Pavel Efimov <wowmanki@mail.ru>
2019-04-15 22:37:20 +0300

    Downloads

--- services.py
+++ services.py
@@ -111,7 +111,7 @@ class Services:
 
     def make_file(self, code):
         filename, received_file = self._PARSER.download_torrent_file(code, self._DATA.get(code)['Description'])
-        file = StringIO()
+        file = BytesIO()
         file.write(received_file)
         file.seek(0, 0)
         return file, filename
--- uparser_bot.py
+++ uparser_bot.py
@@ -177,7 +177,9 @@ def callback_download(call):
     code = call.data.split('_')[1]
     if 'download_' in call.data:
         file, filename = services.make_file(code)
-        bot.send_document(call.from_user.id, file).wait()
+        torrent = bot.download_file(file)
+        bot.send_document(call.from_user.id, torrent).wait()
+
 
     # if 'download_' in call.data:
     #     torrent_path = services.get_path(code)
